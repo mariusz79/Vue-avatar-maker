b691cc3fd03a245c7adfe11a0a86a374874cf0e9
mariusz79 <mpawlowic@gmail.com>
2020-03-15 17:47:19 +0000

    Connect app to api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,21 +4,33 @@
       <input name="input" id="input" v-model="inputValue" @keyup.enter="handleInput"/>
       <button class="generate-button">Generate</button>
     </div>
+     <div class="result" v-if="result">
+      <img v-bind:src=result>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'App',
   data() {
     return {
       inputValue: '',
-      results: [],
+      result: [],
     };
   },
   methods: {
     handleInput() {
-      console.log(this.inputValue);
+      axios.get(`https://eu.ui-avatars.com/api/?name=${this.inputValue}`)
+        .then((response) => {
+          this.result = (response.config.url);
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
   },
 };
